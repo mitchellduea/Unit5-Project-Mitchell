170943d6e34377cd4d4d7a86cffcc37ad25e41df
mitchellduea <54951211+mitchellduea@users.noreply.github.com>
2020-01-16 22:23:04 -0800

    Add files via upload

--- Num.java
+++ Num.java
@@ -1,6 +1,19 @@
 public class Num{
+  private int num;
+  private int getNum1(){
+    return numAmmount;
+  }
+
+private int setNum1(){
+  return numAmmount;
+}
+public num toString(){
+  string Num1();
+}
+public compareTo(num1, num){
+  num1== num;
 
 
 
-  
+}
 }
